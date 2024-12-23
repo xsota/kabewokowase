dfb47a3bd43db79bf5d112bc0044c4b55f01f4b6
sota <i@xsota.me>
2024-12-23 17:52:00 +0900

    カウントダウン機能を追加
    ゲーム開始前のカウントダウン機能を実装し、UIのテキスト表示と音声を連動させました。また、関連するオーディオファイルとフォントをプロジェクトに追加しました。

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,16 +1,24 @@
 extends Node
 
+var is_game = false;
+@export var start_count = 3;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
+	$StartTimer.start()
+	$StartTimer/SE.play()
+	$UI/StartCountDownLabel.text = str(start_count);
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
+	if !is_game:
+		return
+		
 	if event.is_action_pressed("attack"):
 		$Character.play("attack")
 		$Character/SE.play()
@@ -30,3 +38,18 @@ func get_first_wall():
 	
 	return $GroupWall.get_children()[index]
 	
+
+func _on_start_timer_timeout() -> void:
+	start_count -= 1;
+	
+	if start_count >= 1:
+		$UI/StartCountDownLabel.text = str(start_count);
+		$StartTimer/SE.play()
+	elif start_count == 0:
+		$UI/StartCountDownLabel.text = "れんだしろ！！"
+		is_game = true;
+		$BGM.play();
+	elif start_count == -1:
+		$StartTimer.stop()
+		$UI/StartCountDownLabel.hide()
+		
